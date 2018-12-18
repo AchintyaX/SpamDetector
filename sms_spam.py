45d58b3a024d9ec9ca2b343cac57633626a85760
AchintyaX <achintyashankhdhar@gmail.com>
2018-12-19 02:14:06 +0530

    python based tool for checking if a message is spam or not

---
+++ sms_spam.py
@@ -0,0 +1,54 @@
+# importing the libraries 
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+
+# loading the dataset
+
+df = pd.read_csv('spam.csv', encoding='ISO-8859-1')
+
+# dropping the unnecessary columns
+
+df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+
+# rename the columns to something else
+
+df.columns = ['labels', 'data']
+
+# creating a binary mapping 
+df['b_labels']=  df['labels'].map({'spam':1, 'ham':0})
+Y = df['b_labels'].values 
+ 
+# converting the dataset from text to feature vectors using the TF-IDF algorithm
+
+vectorizer = TfidfVectorizer(decode_error='ignore')
+
+X = vectorizer.fit_transform(df['data'])
+
+X_train, X_test, y_train, y_test  = train_test_split(X, Y, test_size=0.33)
+
+# training the model
+
+model1 = MultinomialNB()
+model1.fit(X_train, y_train)
+score1 = model1.score(X_test, y_test)
+
+model2 = AdaBoostClassifier()
+model2.fit(X_train, y_train)
+score2 = model2.score( X_test, y_test)
+
+if(score1>score2):
+	predictions = model1.predict(X_test)
+else:
+	predictions = model2.predict(X_test)
+
+correct = 0
+for i in range(0, len(y_test)):
+	if predictions[i] == y_test[i]:
+		correct = correct+1
+correct_percent = correct/len(predictions)
+print("accuracy of the predictions is:", correct_percent)
