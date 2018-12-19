9617151be78574a3c6cbe2b077bb487e99476d96
AchintyaX <achintyashankhdhar@gmail.com>
2018-12-20 03:04:04 +0530

    python script based classification

--- sms_spam.py
+++ sms_spam.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 # loading the dataset
 
-df = pd.read_csv('spam.csv', encoding='ISO-8859-1')
+df = pd.read_csv('sms_spam.csv', encoding='ISO-8859-1')
 
 # dropping the unnecessary columns
 
@@ -28,6 +28,7 @@ Y = df['b_labels'].values
 vectorizer = TfidfVectorizer(decode_error='ignore')
 
 X = vectorizer.fit_transform(df['data'])
+print(X)
 
 X_train, X_test, y_train, y_test  = train_test_split(X, Y, test_size=0.33)
 
@@ -52,3 +53,10 @@ for i in range(0, len(y_test)):
 		correct = correct+1
 correct_percent = correct/len(predictions)
 print("accuracy of the predictions is:", correct_percent)
+
+# testing on taking live input
+test = input("enter the text: ")
+test1 = ["Python's popularity has propelled once again, with a major driver of its growth being Data Science—one of the fastest growing global business trends. Like yin and yang, Data Science and Python are now two inseparable forces that stand mightily on their own, but together, they produce an even more powerful energy."]
+trial = vectorizer.fit_transform(test1)
+predictions1 = model2.predict(trial)
+print(predictions1)
